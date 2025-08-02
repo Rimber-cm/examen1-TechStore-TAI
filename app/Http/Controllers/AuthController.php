3337d05924c0e93428f3d1cdea8bf428aee63e08
Rimber-cm <calderonrimber@gmail.com>
2025-08-02 19:24:42 -0400

    Implementar el registro de usuarios

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User; // Importa el modelo de usuario
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        // Crear el nuevo usuario
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password), // ¡Importante! Encriptar la contraseña
+        ]);
+        
+        // Crear un token de autenticación para el usuario
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        // Devolver una respuesta JSON con el token
+        return response()->json([
+            'message' => 'Usuario registrado con éxito',
+            'user' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
 }
